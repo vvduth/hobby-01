cf1e2e4412505cfa446b11ed4497eb446fac21f7
vvduth <ducthai060501@gmail.com>
2025-04-15 15:04:33 +0300

    Some Navbar and ract router frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,27 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Button } from '@chakra-ui/react'
+import { Box, Button } from '@chakra-ui/react'
+import { Route, Routes } from 'react-router-dom'
+import CreatePage from './pages/CreatePage'
+import HomePage from './pages/HomePage'
+import Navbar from './component/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <Button>
-        hello 
-      </Button>
+      <Box minH={"100vh"}>
+        {/* nav */}
+        <Navbar/>
+
+        <Routes>
+          <Route path='/' element={<><HomePage/></>}></Route>
+          <Route path='/create' element={<CreatePage/>}></Route>
+
+        </Routes>
+      </Box>
     </>
   )
 }
