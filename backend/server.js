54d73e3e09f8d7baf7448ecc0331ce142b1211bd
vvduth <ducthai060501@gmail.com>
2025-04-16 09:53:55 +0300

    add some config for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-import Products from "./model/product.model.js";
+import path from "path"
 import productRoutes from "./routes/product.route.js"
+
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000
+const __dirname = path.resolve()
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.use(express.json()); // allow ous to accept json data as body
-dotenv.config();
+
 app.get("/", (req, res) => {
   res.send("server is ready brother");
 });
@@ -15,6 +25,6 @@ app.get("/", (req, res) => {
 app.use("/api/products", productRoutes)
 
 app.listen(PORT, () => {
-  connectDB();
-  console.log("server started at port 5000");
+	connectDB();
+	console.log("Server started at http://localhost:" + PORT);
 });
